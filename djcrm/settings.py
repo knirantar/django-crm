24edad194ddf9b41ed577973e815340fa580f52d
Nirantar Kulkarni <knirantar@gmail.com>
2021-03-29 18:25:50 +0530

    email masked

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "knirantar@gmail.com"
-EMAIL_HOST_PASSWORD = "edkkofsfkkrugxhf" 
+EMAIL_HOST_USER = env("HOST_EMAIL")
+EMAIL_HOST_PASSWORD = env("GMAIL_PASSWORD") 
